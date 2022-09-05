47bb1c7bbd90c0dc6944613fa7a66b6e19227985
leandryncsc <leandrinhodoema@gmail.com>
2022-09-05 13:06:14 -0300

    first commit

---
+++ android/app/src/main/kotlin/br/com/leandro/flutter_mapa/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.leandro.flutter_mapa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/order_traking_page.dart
@@ -0,0 +1,147 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_mapa/constants.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+
+class OrderTrackingPage extends StatefulWidget {
+  const OrderTrackingPage({Key? key}) : super(key: key);
+
+  @override
+  State<OrderTrackingPage> createState() => OrderTrackingPageState();
+}
+
+class OrderTrackingPageState extends State<OrderTrackingPage> {
+  final Completer<GoogleMapController> _controller = Completer();
+
+  static const LatLng sourceLocation = LatLng(-3.861279, -38.599407);
+  static const LatLng destination = LatLng(-3.880535, -38.624570);
+
+  List<LatLng> polylineCoordinates = [];
+  LocationData? currentLocation;
+
+  BitmapDescriptor sourceIcon = BitmapDescriptor.defaultMarker;
+  BitmapDescriptor destinationIcon = BitmapDescriptor.defaultMarker;
+  BitmapDescriptor currentLocationIcon = BitmapDescriptor.defaultMarker;
+
+  void getCurrentLocation() async {
+    Location location = Location();
+
+    location.getLocation().then(
+      (location) {
+        currentLocation = location;
+      },
+    );
+
+    GoogleMapController googleMapController = await _controller.future;
+
+    location.onLocationChanged.listen(
+      (newLoc) {
+        currentLocation = newLoc;
+
+        googleMapController.animateCamera(
+          CameraUpdate.newCameraPosition(
+            CameraPosition(
+              zoom: 13.5,
+              target: LatLng(
+                newLoc.latitude!,
+                newLoc.longitude!,
+              ),
+            ),
+          ),
+        );
+
+        setState(() {});
+      },
+    );
+  }
+
+  void getPolypoints() async {
+    PolylinePoints polylinePoints = PolylinePoints();
+    PolylineResult result = await PolylinePoints().getRouteBetweenCoordinates(
+      google_api_key,
+      PointLatLng(sourceLocation.latitude, sourceLocation.longitude),
+      PointLatLng(destination.latitude, destination.longitude),
+    );
+
+    if (result.points.isEmpty) {
+      result.points.forEach(
+        (PointLatLng point) => polylineCoordinates.add(
+          LatLng(point.latitude, point.longitude),
+        ),
+      );
+      setState(() {});
+    }
+  }
+
+  void setCustomMarkerIcon() {
+    BitmapDescriptor.fromAssetImage(ImageConfiguration.empty, "assets/")
+        .then((icon) {
+      sourceIcon = icon;
+    });
+    BitmapDescriptor.fromAssetImage(ImageConfiguration.empty, "assets/")
+        .then((icon) {
+      destinationIcon = icon;
+    });
+    BitmapDescriptor.fromAssetImage(ImageConfiguration.empty, "assets/")
+        .then((icon) {
+      currentLocationIcon = icon;
+    });
+  }
+
+  @override
+  void initState() {
+    getCurrentLocation();
+    getPolypoints();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Mapa em Flutter",
+          style: TextStyle(color: Colors.black, fontSize: 16),
+        ),
+      ),
+      body: currentLocation == null
+          ? const Center(child: Text("loading"))
+          : GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: LatLng(
+                    currentLocation!.latitude!, currentLocation!.longitude!),
+                zoom: 13.5,
+              ),
+              polylines: {
+                Polyline(
+                  polylineId: PolylineId("route"),
+                  points: polylineCoordinates,
+                  color: primaryColor,
+                  width: 6,
+                ),
+              },
+              markers: {
+                Marker(
+                  markerId: const MarkerId("currentLocation"),
+                  icon: currentLocationIcon,
+                  position: LatLng(
+                      currentLocation!.latitude!, currentLocation!.longitude!),
+                ),
+                const Marker(
+                  markerId: MarkerId("source"),
+                  position: sourceLocation,
+                ),
+                const Marker(
+                  markerId: MarkerId("destination"),
+                  position: destination,
+                ),
+              },
+              onMapCreated: (mapController) {
+                _controller.complete(mapController);
+              },
+            ),
+    );
+  }
+}
